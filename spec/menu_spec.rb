8b53674d3686f6531576c7637ece9bdd7ffeb4be
jamierose <jrose111284@gmail.com>
2016-01-23 19:49:59 +0000

    prints a list of dishes with prices

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -4,12 +4,17 @@ describe Menu do
   subject(:menu) { described_class.new(dishes) }
   let (:dishes) do
     {
-      chicken: 3.50,
-      falafel: 4.50
+      Chicken: 3.50,
+      Falafel: 4.50
     }
   end
 
   it 'has a list of dishes with prices' do
     expect(menu.dishes).to eq(dishes)
+  end
+
+  it 'prints a list of dishes with prices' do
+    printed_menu = "Chicken £3.50, Falafel £4.50"
+    expect(menu.print).to eq(printed_menu)
   end
 end
