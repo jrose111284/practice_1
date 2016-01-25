3658dea65df91abfb6fa8c23ba973c437fad3d28
jamierose <jrose111284@gmail.com>
2016-01-25 16:01:25 +0000

    tells if a dish is not on the menu

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -21,4 +21,8 @@ describe Menu do
   it "tells if a dish is on the menu" do
     expect(menu.has_dish?(:Chicken)).to be(true)
   end
+
+  it "tells if a dish is not on the menu" do
+    expect(menu.has_dish?(:beef)).to be(false)
+  end
 end
